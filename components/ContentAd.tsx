9e8b3d58a4cfcb30c08ccb142361d2d6d6d7bc01
Belly <220792825+NiceBelly@users.noreply.github.com>
2025-12-09 13:19:56 +0900

    Fix: Add missing ContentAd component

---
+++ components/ContentAd.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const ContentAd: React.FC = () => {
+  return (
+    <div className="w-full max-w-4xl mx-auto my-8">
+      {/* 광고 영역 - 필요시 광고 코드를 추가하세요 */}
+      <div className="bg-gray-100 dark:bg-gray-800 rounded-lg p-4 text-center text-gray-500 dark:text-gray-400">
+        {/* Advertisement space */}
+      </div>
+    </div>
+  );
+};
+
+export default ContentAd;
